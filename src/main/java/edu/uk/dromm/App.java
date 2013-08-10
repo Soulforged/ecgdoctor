4c1f79df90e9114deb5bf226e3a90ca2613974f3
Daniel Icardo <icardo.daniel@gmail.com>
2013-08-10 16:45:23 -0300

    Fuentes y pom

---
+++ src/main/java/edu/uk/dromm/App.java
@@ -0,0 +1,13 @@
+package edu.uk.dromm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
