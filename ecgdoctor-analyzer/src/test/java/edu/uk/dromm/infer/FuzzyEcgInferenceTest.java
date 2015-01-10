356ec130572ce2cede1a20ecaa8f65e642909c51
Mariano Silva <marianoLuissilva@gmail.com>
2015-01-10 15:53:16 -0300

    como andan los tests!!!!

--- ecgdoctor-analyzer/src/test/java/edu/uk/dromm/infer/FuzzyEcgInferenceTest.java
+++ ecgdoctor-analyzer/src/test/java/edu/uk/dromm/infer/FuzzyEcgInferenceTest.java
@@ -1,9 +1,11 @@
 package edu.uk.dromm.infer;
 
 import java.net.URL;
+import java.util.HashMap;
 
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.rule.LinguisticTerm;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 import org.junit.Assert;
@@ -32,10 +34,7 @@ public class FuzzyEcgInferenceTest {
   public void sureRegularSinusRythm(){
     final FunctionBlock diagnosis = fis.getFunctionBlock("diagnosis");
     diagnosis.setVariable("rr", 1335);
-    diagnosis.setVariable("pv", 10);
-    diagnosis.setVariable("qrs", 120);
-    diagnosis.setVariable("pr", 160);
-    assertInputRanges(diagnosis, 1.0, 1.0, 1.0, 1.0);
+    assertInputRanges(diagnosis, 1.0);
     diagnosis.evaluate();
     final Variable result = diagnosis.getVariable("result");
     Assert.assertTrue(result.getValue() > 0.9);
@@ -45,46 +44,52 @@ public class FuzzyEcgInferenceTest {
   public void borderlineRegularSinusRythm(){
     final FunctionBlock diagnosis = fis.getFunctionBlock("diagnosis");
     diagnosis.setVariable("rr", 1000);
-    diagnosis.setVariable("pv", 0.84);
-    diagnosis.setVariable("qrs", 110);
-    diagnosis.setVariable("pr", 120);
-    assertInputRanges(diagnosis, 0.6, 0.6, 0.6, 0.6);
+    assertInputRanges(diagnosis, 0.6);
     diagnosis.evaluate();
     Variable result = diagnosis.getVariable("result");
-    System.out.println(result.getValue());
-    Assert.assertTrue(result.getValue() > 0.5);
+    Assert.assertNotNull(result.getLinguisticTerm("normalsr"));
     diagnosis.setVariable("rr", 1670);
-    diagnosis.setVariable("pv", 0.84);
-    diagnosis.setVariable("qrs", 130);
-    diagnosis.setVariable("pr", 200);
-    assertInputRanges(diagnosis, 0.6, 0.6, 0.6, 0.6);
     diagnosis.evaluate();
     result = diagnosis.getVariable("result");
-    System.out.println(result.getValue());
-    Assert.assertTrue(result.getValue() > 0.5);
+    Assert.assertNotNull(result.getLinguisticTerm("normalsr"));
+  }
+
+  @Test
+  public void cardiopathy(){
+	  final FunctionBlock diagnosis = fis.getFunctionBlock("diagnosis");
+	  diagnosis.setVariable("rr", 750);
+	  diagnosis.evaluate();
+	  Variable result = diagnosis.getVariable("result");
+	  Assert.assertNotNull(result.getLinguisticTerm("cardiopathy"));
+	  diagnosis.setVariable("rr", 2000);
+	  diagnosis.evaluate();
+	  Assert.assertNotNull(result.getLinguisticTerm("cardiopathy"));
+  }
+
+  @Test
+  public void cardiopathyLimits(){
+	  final FunctionBlock diagnosis = fis.getFunctionBlock("diagnosis");
+	  diagnosis.setVariable("rr", 950);
+	  diagnosis.evaluate();
+	  Variable result = diagnosis.getVariable("result");
+	  Assert.assertNotNull(result.getLinguisticTerm("cardiopathy"));
+	  diagnosis.setVariable("rr", 1750);
+	  diagnosis.evaluate();
+	  Assert.assertNotNull(result.getLinguisticTerm("cardiopathy"));
   }
 
   public void sinusBradycardiaRythm(){
     final FunctionBlock diagnosis = fis.getFunctionBlock("diagnosis");
     diagnosis.setVariable("rr", 1335);
-    diagnosis.setVariable("pv", 10);
-    diagnosis.setVariable("qrs", 120);
-    diagnosis.setVariable("pr", 160);
-    assertInputRanges(diagnosis, 1.0, 1.0, 1.0, 1.0);
+    assertInputRanges(diagnosis, 1.0);
     diagnosis.evaluate();
     final Variable result = diagnosis.getVariable("result");
     Assert.assertTrue(result.getValue() > 0.9);
   }
 
-  private void assertInputRanges(final FunctionBlock fb, final double rrexp, final double qrsexp, final double pvexp, final double prexp){
+  private void assertInputRanges(final FunctionBlock fb, final double rrexp){
     final double rrmemb = fb.getVariable("rr").getMembership("normal");
     Assert.assertEquals("RR membership should be near " + rrexp, rrexp, rrmemb, 0.01);
-    final double pvmemb = fb.getVariable("pv").getMembership("present");
-    Assert.assertEquals("PV membership should be near " + pvexp, pvexp, pvmemb, 0.01);
-    final double qrsmemb = fb.getVariable("qrs").getMembership("normal");
-    Assert.assertEquals("QRS membership should be near " + qrsexp, qrsexp, qrsmemb, 0.01);
-    final double prmemb = fb.getVariable("pr").getMembership("normal");
-    Assert.assertEquals("PR membership should be near " + prexp, prexp, prmemb, 0.01);
   }
 
   @Test
